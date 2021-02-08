475782655ccbaa2eb17f89756e045d7d70604165
Annabendahan <an.bendahan@gmail.com>
2021-02-08 18:52:03 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,44 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import gif from '../images/maillotsmarie.gif'
+import dash from '../images/dash.svg'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div className="main">
+      <br />
+      <div className="main__gif">
+        <img alt="img" src={gif} />
+      </div>
+      <br /><br /><br /><br />
+      <div className="main__list">
+        <div className="main__list__line">
+          <p> Le maillot </p>
+          <img alt="img" src={dash} />
+          <h5> Couture </h5>
+        </div>
+        <div className="main__list__line">
+          <p> La coloc </p>
+          <img alt="img" src={dash} />
+          <h5> Divers </h5>
+        </div>
+        <div className="main__list__line">
+          <p> La collec </p>
+          <img alt="img" src={dash} />
+          <h5> Maillots </h5>
+        </div>
+      </div>
+      <div className="main__bar">
+        <p> © 2021 </p>
+        <h5> <span className='main__bar__fc'>FC</span>_Magenta </h5>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+
   </Layout>
 )
 
